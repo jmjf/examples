2b0177a5d32717ba80e36517c761d79c4544596b
Oliver Buchtala <oliver.buchtala@gmail.com>
2016-03-09 18:59:45 +0100

    Styling and click handler for AlienNode.

--- isolated-nodes/alien/AlienComponent.js
+++ isolated-nodes/alien/AlienComponent.js
@@ -17,7 +17,31 @@ AlienComponent.Prototype = function() {
     if (this.props.node.mood) {
       el.addClass('sm-' + this.props.node.mood);
     }
+
+    var overlay = $$('div').addClass('se-overlay')
+      .append($$('button').append('Click Here').on('mousedown', this.onClick));
+    el.append(overlay);
+
     return el;
+  };
+
+  var _moods = ['normal', 'angry', 'excited', 'sad', 'sick'];
+
+  this.onClick = function(event) {
+    event.preventDefault();
+    event.stopPropagation();
+
+    var surface = this.context.surface;
+    var node = this.props.node;
+
+    var mood = node.mood || 'normal';
+    var idx = _moods.indexOf(mood);
+    idx = (idx+1) % _moods.length;
+    mood = _moods[idx];
+    surface.transaction(function(tx) {
+      tx.set([node.id, 'mood'], mood);
+    });
+    this.rerender();
   };
 
 };
